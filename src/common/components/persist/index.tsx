be241a74d6d6805bd4f3fb9a63999ea46261dcbf
emammad22 <mammadoveltun22@gmail.com>
2024-04-21 23:05:58 +0400

    sign-in loader added

--- src/common/components/persist/index.tsx
+++ src/common/components/persist/index.tsx
@@ -5,5 +5,6 @@ import Spinner from "../spinner";
 export default function Persist() {
   const { isAuth, isUserLoading } = useAuthStore();
   if (isAuth === undefined || isUserLoading) return <Spinner />;
+
   return isAuth ? <Outlet /> : <Navigate to="/login" replace />;
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,24 +2,31 @@ import { useNavigate } from "react-router-dom";
 import SignInForm from "./components/sign-in-form";
 import useAuthStore from "@/services/store/authStore";
 import { useEffect } from "react";
+import Spinner from "@/common/components/spinner";
 
 const Login = () => {
   const navigate = useNavigate();
-  const { isAuth } = useAuthStore();
+  const { isAuth, isSignInLoading } = useAuthStore();
 
   useEffect(() => {
-    console.log('isAuth', isAuth)
     isAuth && navigate("/");
   }, [isAuth, navigate]);
+
   return (
-    <div className="relative font-inter">
-      <div className="bg-contain  bg-no-repeat w-full h-screen absolute top-0 left-0 bg-[url('/images/LoginBG.svg')]">
-        <div className="flex h-full ">
-          <div className="bg-[#068F84C4] w-1/2 h-full bg-opacity-75"></div>
-          <SignInForm />
+    <>
+      {!isSignInLoading ? (
+        <div className="relative font-inter">
+          <div className="bg-contain  bg-no-repeat w-full h-screen absolute top-0 left-0 bg-[url('/images/LoginBG.svg')]">
+            <div className="flex h-full ">
+              <div className="bg-[#068F84C4] w-1/2 h-full bg-opacity-75"></div>
+              <SignInForm />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <Spinner />
+      )}
+    </>
   );
 };
 
