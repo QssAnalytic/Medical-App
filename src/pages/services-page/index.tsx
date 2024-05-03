cd5a8f250410d96e38ba25f28b36a2a7d7f3d704
emammad22 <mammadoveltun22@gmail.com>
2024-05-03 17:11:29 +0400

    fixed design problem

--- src/pages/services-page/index.tsx
+++ src/pages/services-page/index.tsx
@@ -1,19 +1,23 @@
-import ServicesBars from "./service-bars"
-import ServicesCharts from "./service-charts"
+import ServicesBars from "./service-bars";
+import ServicesCharts from "./service-charts";
 
 const Services = () => {
   return (
     <div className="border border-mainBorder bg-chartBg rounded-lg px-4 py-3 relative ml-3 mb-10 h-full">
-      <div> 
-        <p className="absolute transform -rotate-90  w-72  text-center text-base text-mainText font-medium right-[91%] top-[24%]">Services  Comparsion</p>
+      <div className="relative">
+        <p className="absolute transform -rotate-90  w-72  text-center text-base text-mainText font-medium left-[-180px] top-[47%]">
+          Hospitals Comparsion
+        </p>
         <ServicesBars />
-      </div> 
-      <div>
-        <p className="absolute transform -rotate-90  w-72  text-center text-base text-mainText font-medium right-[91%] top-[78%]" >Time Series Analysis</p>
+      </div>
+      <div className="relative">
+        <p className="absolute transform -rotate-90  w-72  text-center text-base text-mainText font-medium left-[-180px] top-[47%]">
+          Time Series Analysis
+        </p>
         <ServicesCharts />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
